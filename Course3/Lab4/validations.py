fe7c3bac13789a0bea7afbc900d6766f85ae2a54
Steve <murimi101@gmail.com>
2020-10-14 11:59:55 +0000

    "Closes: #1" Updated validations.py python script. I have created my first python function to check if a username starts with a letter

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -20,5 +20,12 @@ def validate_user(username, minlen):
         return False
     return True
 
+	#Usernames must start with a letter
+	first_char = username[0]
+	if username.isalpha():
+	return True
 
-
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
